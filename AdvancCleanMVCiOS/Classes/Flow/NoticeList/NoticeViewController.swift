ce0842c26037718f4e208d3f895c5d4bf5aa027a
Anamika Deb <anamika@seazoobd.com>
2020-10-20 12:55:39 +0600

    Create new files for Notice List

---
+++ AdvancCleanMVCiOS/Classes/Flow/NoticeList/NoticeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeViewController.swift
+//  AdvancCleanMVCiOS
+//
+//  Created by Anamika Deb on 20/10/20.
+//  Copyright © 2020 Anamika Deb. All rights reserved.
+//
+
+import Foundation
