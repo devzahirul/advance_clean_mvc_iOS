777fda8adfa8433b249299b7093203ba7ec181ad
Anamika Deb <anamika@seazoobd.com>
2020-10-22 09:11:31 +0600

    New files for notice details.

---
+++ AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsRouter.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeDetailsRouter.swift
+//  AdvancCleanMVCiOS
+//
+//  Created by Anamika Deb on 22/10/20.
+//  Copyright © 2020 Anamika Deb. All rights reserved.
+//
+
+import Foundation
---
+++ AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NoticeDetailsViewController.swift
+//  AdvancCleanMVCiOS
+//
+//  Created by Anamika Deb on 22/10/20.
+//  Copyright © 2020 Anamika Deb. All rights reserved.
+//
+
+import Foundation
