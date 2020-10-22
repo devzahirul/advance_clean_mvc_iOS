d2d528cee28650a309d5ef4bb02c6e16229dcf55
Anamika Deb <anamika@seazoobd.com>
2020-10-22 12:00:22 +0600

    Design & show Notice details.

--- AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsRouter.swift
+++ AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsRouter.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 Anamika Deb. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class NoticeDetailsRouter: NoticeListPresenter {
+    var currentViewController: UIViewController!
+    
+    var serviceLocator: ServiceLocator!
+    
+    
+    init() {
+        //init service
+        serviceLocator = ServiceLocator.init()
+    }
+    
+}
+
+extension NoticeDetailsRouter : Router{
+    // Create & show signin view
+    func execute(context: UIViewController, info: AnyObject?) {
+        
+        //check your opening logic
+        //setup router
+        let noticeVC = NoticeDetailsViewController.create()
+        noticeVC.modalPresentationStyle = .fullScreen
+        noticeVC.notice = info as? NoticeModel
+//        noticeVC.setRouter(router: self)
+        currentViewController = noticeVC
+        context.present(noticeVC, animated: false, completion: nil)
+    }
+}
--- AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsViewController.swift
+++ AdvancCleanMVCiOS/Classes/Flow/NoticeDetail/NoticeDetailsViewController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 Anamika Deb. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class NoticeDetailsViewController: UIViewController, StoryboardInitable {
+    
+    static var storyboardName: String = "NoticeDetails"
+    var notice: NoticeModel? = nil
+    
+    @IBOutlet weak var companyNameLbl: UILabel!
+    @IBOutlet weak var noticeNameLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var noticeTypeLbl: UILabel!
+    @IBOutlet weak var noticeDescriptionLbl: UILabel!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Private methods
+    private func setupView() {
+        companyNameLbl.text = notice?.companyName
+        noticeNameLbl.text = notice?.noticeName
+        dateLbl.text = notice?.createDate
+        noticeTypeLbl.text = notice?.noticeCategory
+        noticeDescriptionLbl.text = notice?.description
+    }
+    
+    //MARK: IBACTION
+    @IBAction func clickedBackButton(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
+}
